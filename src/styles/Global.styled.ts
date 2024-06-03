8146d16ce196f240e1ad90042c7569203ccc8e6d
Efimov Yaroslav <ya.efimov@internet.ru>
2024-06-03 23:07:59 +0300

    + Start part 2

---
+++ src/styles/Global.styled.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+import { Theme } from "./Theme";
+
+export const GlobalStyled = createGlobalStyle`
+
+    *,
+    *::before,
+    *::after{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+body {
+  margin: 0;
+  font-family: 'Manrope',  -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: ${Theme.colors.fontTitle};
+}
+
+a{
+    text-decoration: none; /* убираем подчеркивание у ссылок */
+}
+
+ul{
+    list-style: none;
+}
+
+button{
+    background-color: unset;
+    border: none;
+}
+
+section:nth-of-type(odd){ /* для четных секций */
+    background-color: ${Theme.colors.primaryBg};
+}
+
+section:nth-of-type(even){ /* для нечетных секций */
+    background-color: ${Theme.colors.primaryBg};
+}
+
+`;
---
+++ src/styles/Theme.ts
@@ -0,0 +1,9 @@
+export const Theme = {
+  colors: {
+    primaryBg: "#ffffff",
+    secondaryBg: "#252527",
+    buttonBg: "#181818",
+
+    fontTitle: "#F8F8F8",
+  },
+};
